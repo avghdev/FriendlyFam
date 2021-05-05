03db5b532a0f43cb4e0990fe969ad4b2cb6b9b25
Dumplin <avghdev@gmail.com>
2021-05-04 20:28:31 -0400

    Initial Commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session
 import mysql.connector
+import gunicorn
 
 app = Flask(__name__)
 app.secret_key = "secret"
@@ -172,10 +173,4 @@ def logout():
 
 
 if __name__ == '__main__':
-    import os
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
     app.run(HOST, PORT)
